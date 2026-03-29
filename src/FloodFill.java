6da8aa2d432070099d34d8294d757300422d89ae
Zhou <zhoukashen@gmail.com>
2026-03-29 00:23:01 -0300

    Adicionando novos arquivos.
    Atualização do Main, adicionando FloodFill.java e Ponto.java

---
+++ src/FloodFill.java
@@ -0,0 +1,101 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class FloodFill {
+
+    public void FloodFillPilha(BufferedImage img, int x, int y, int corOriginal, int novaCor) {
+        Stack<Ponto> stack = new Stack<>(); // vai controlar quais pixels precisam ser controlados
+        stack.push(new Ponto(x, y));
+
+        if (corOriginal == novaCor) {
+            return;
+        }
+
+        int contadorPilha = 0; // vai controlar os frames
+
+        File pasta = new File("frames");
+        if (!pasta.exists()) {
+            pasta.mkdirs();
+        } // cria pasta se nao existir
+
+        while (!stack.isEmpty()) {
+            Ponto p = stack.pop();
+
+            if (p.getX() < 0 || p.getY() < 0 || p.getX() >= img.getWidth() || p.getY() >= img.getHeight()) {
+                continue;
+            } // verifica limites
+
+            if (img.getRGB(p.getX(), p.getY()) != corOriginal) {
+                continue;
+            } // verifica cor
+
+            img.setRGB(p.getX(), p.getY(), novaCor); // pinta pixel
+
+            try {
+                contadorPilha++;
+                if (contadorPilha % 50 == 0) {
+                    ImageIO.write(img, "png", new File("frames/frame_" + contadorPilha + ".png"));
+                } // ele vai salvar a cada 50 pixels
+
+            } catch (IOException e) {
+                e.printStackTrace();
+
+            }
+
+            // vizinhos
+            stack.push(new Ponto(p.getX() + 1, p.getY()));
+            stack.push(new Ponto(p.getX() - 1, p.getY()));
+            stack.push(new Ponto(p.getX(), p.getY() + 1));
+            stack.push(new Ponto(p.getX(), p.getY() - 1));
+
+        }
+    }
+
+    public void FloodFillFila(BufferedImage img, int x, int y, int corOriginal, int novaCor) {
+
+        Queue<Ponto> queue = new Queue<>();
+        queue.enqueue(new Ponto(x, y));
+
+        if (corOriginal == novaCor) return;
+
+        int contadorFila = 0;
+
+        File pasta = new File("frames");
+        if (!pasta.exists()) {
+            pasta.mkdirs();
+        } // cria pasta se nao existir
+
+        while (!queue.isEmpty()) {
+
+            Ponto p = queue.dequeue();
+
+            if (p.getX() < 0 || p.getY() < 0 || p.getX() >= img.getWidth() || p.getY() >= img.getHeight()) {
+                continue;
+            }
+
+            if (img.getRGB(p.getX(), p.getY()) != corOriginal) {
+                continue;
+            }
+
+            img.setRGB(p.getX(), p.getY(), novaCor);
+
+            try {
+                contadorFila++;
+                if (contadorFila % 50 == 0) {
+                    ImageIO.write(img, "png", new File("frames/frame_" + contadorFila + ".png"));
+                } // ele vai salvar a cada 50 pixels
+
+            } catch (IOException e) {
+                e.printStackTrace();
+
+            }
+
+            queue.enqueue(new Ponto(p.getX() + 1, p.getY()));
+            queue.enqueue(new Ponto(p.getX() - 1, p.getY()));
+            queue.enqueue(new Ponto(p.getX(), p.getY() + 1));
+            queue.enqueue(new Ponto(p.getX(), p.getY() - 1));
+        }
+    }
+}
--- src/Pilha.java
+++ src/Pilha.java
@@ -70,3 +70,4 @@ class Stack<T> {
         return s;
     }
 }
+
